977fa41c380399e7efa2eace434769ea1e68eaa9
PLeh2023 <philipp.lehmann@nexus-ag.de>
2024-03-28 09:07:36 +0100

    Day 2 with comments and changes to Day 1

--- src/Day1.java
+++ src/Day1.java
@@ -8,12 +8,14 @@ public class Day1 {
         //            the first and last number in a row are one combined number
         //The methode "wordToNumber" is only needed for the 2nd problem in which the words need to be numbers
 
-        int zahl = input();
-        System.out.println(zahl);
+        //valid numbers are 1 and 2 which are representing the problems
+        int finalNumberOne = input(1);
+        int finalNumberTwo = input(2);
+        System.out.println("First Problem: " + finalNumberOne);
+        System.out.println("Second Problem: " + finalNumberTwo);
     }
-    public static int input(){
-
-        int zahl = 0;
+    public static int input(int choice){
+        int finalNumber = 0;
         //try-catch: It could be possible that the mentioned File doesn´t exist and that would break the programm
         try {
             //selecting of the file
@@ -24,17 +26,22 @@ public class Day1 {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line;
-            while((line = bufferedReader.readLine()) != null ){
-                line = wordToNumber(line);
-                zahl += findNumbers(line);
+            if (choice == 1){
+                while((line = bufferedReader.readLine()) != null ){
+                    finalNumber += findNumbers(line);
+                }
+            }else{
+                while((line = bufferedReader.readLine()) != null ){
+                    line = wordToNumber(line);
+                    finalNumber += findNumbers(line);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return zahl;
+        return finalNumber;
     }
     public static int findNumbers(String line){
-
         int firstNumber = 0;
         int lastNumber = 0;
         int finalNumber;
@@ -50,7 +57,6 @@ public class Day1 {
                 lastNumber = Integer.parseInt(character);
             }
         }
-
         String tmp = "";
         tmp += firstNumber;
         tmp += lastNumber;
@@ -61,13 +67,10 @@ public class Day1 {
         // replacing the character sequences with another one because there are combinations like threeight
         // which would be 3 and 8 but with a full word replacement from three to 3 then the eight wouldn´t
         // be recognized anymore
-        String newLine;
-        newLine =   line.replace("one","on1e").replace("two","tw2o")
+        return  line.replace("one","on1e").replace("two","tw2o")
                     .replace("three","thr3ee").replace("four","fo4ur")
                     .replace("five","fi5ve").replace("six","si6x")
                     .replace("seven","sev7en").replace("eight","eig8ht")
                     .replace("nine","ni9ne");
-
-        return newLine;
     }
 }
---
+++ src/Day2.java
@@ -0,0 +1,122 @@
+import java.io.*;
+
+public class Day2 {
+    public static void main(String[] args){
+
+        //1. Problem:   Finding all games in which the maximum amount of cubes for any colour is not exceeded.
+        //              Then adding all game IDs like "Game 5" would be 5 together
+        //2. Problem:   Finding in each game the maximum amount of cubes for any colour and
+        //              then multiplying them together. The game number is not important now.
+
+        int sum = input(1);
+        int pow = input(2);
+        System.out.println("First Problem: " + sum);
+        System.out.println("Second Problem: " + pow);
+    }
+    public static int input(int choice){
+        int sum = 0;
+        try {
+            File file = new File("Inputs/Day2_Input");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            if(choice == 1) {
+                while ((line = bufferedReader.readLine()) != null) {
+                    line = normalizeInput(line);
+                    sum += possibleGames(line);
+                }
+            }else{
+                while ((line = bufferedReader.readLine()) != null) {
+                    line = normalizeInput(line);
+                    sum += powerOfGame(line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sum;
+    }
+    public static String normalizeInput(String line){
+        //From: Game 13: 2 blue, 5 green; 2 blue, 2 green; 2 blue, 2 red, 4 green
+        //To:   13;2b;5g;2b;2g;2b;2r;4g
+        return  line.replace("Game ","").replace(":",";")
+                    .replace("red","r").replace("blue","b")
+                    .replace("green","g").replace(" ","")
+                    .replace(",",";");
+    }
+    public static int possibleGames(String line){
+        int game;
+        int cubeCount;
+        boolean red = true,green = true,blue = true;
+        String[] cubes;
+        //splitting each line into their cube count
+        cubes = line.split(";");
+        //getting the game ID for the sum of the problem
+        game = Integer.parseInt(cubes[0]);
+        //filter for the colour in each part of the line
+        for(String i: cubes){
+            if(i.contains("r")){
+                cubeCount = getNumberFromString(i);
+                //deciding if the game is with the amount of cubes possible
+                if(cubeCount > 12){
+                    red = false;
+                }
+            }else if(i.contains("g")){
+                cubeCount = getNumberFromString(i);
+                if(cubeCount > 13){
+                    green = false;
+                }
+            }else if(i.contains("b")){
+                cubeCount = getNumberFromString(i);
+                if(cubeCount > 14){
+                    blue = false;
+                }
+            }
+        }
+        //return the game ID if the game is successful else just return 0
+        if(red && green && blue){
+            return game;
+        }else{
+            return 0;
+        }
+    }
+    public static int getNumberFromString(String i){
+        int number;
+        //getting the length of the Strings, so that the number can be extracted
+        if(i.length() == 2){
+            number = Integer.parseInt("" + i.charAt(0));
+        }else{
+            number = Integer.parseInt("" + i.charAt(0) + i.charAt(1));
+        }
+        return number;
+    }
+    public static int powerOfGame(String line){
+        int cubeCount;
+        int red = 0, green = 0, blue = 0;
+        String[] cubes;
+        //splitting each line into their cube count
+        cubes = line.split(";");
+        //filter for the colour in each part of the line
+        for(String i: cubes){
+            if(i.contains("r")){
+                cubeCount = getNumberFromString(i);
+                //finding the biggest amount of cubes of each colour
+                if(cubeCount > red){
+                    red = cubeCount;
+                }
+            }else if(i.contains("g")){
+                cubeCount = getNumberFromString(i);
+                if(cubeCount > green){
+                    green = cubeCount;
+                }
+            }else if(i.contains("b")){
+                cubeCount = getNumberFromString(i);
+                if(cubeCount > blue){
+                    blue = cubeCount;
+                }
+            }
+        }
+        //multiplying all colours together
+        return red * green * blue;
+    }
+}
