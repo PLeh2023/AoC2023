6d0ad8e21a640c6a8dc8deda5d11304040126395
PLeh2023 <philipp.lehmann@nexus-ag.de>
2024-04-02 08:48:14 +0200

    miscellaneous

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,6 +2,7 @@ public class Welcome {
     public static void main(String[] args) {
 
         System.out.println("Welcome to my repository!");
+        System.out.println("===Everything is written in JDK 21.0.2===");
 
     }
 }
